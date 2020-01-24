5545ad4e9419c75f3f335ec0e6c599ddeaf36941
Biggs Thorarensen <biggs@devillion.com>
2020-01-24 09:43:59 +0000

    connecting to db

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
+// required modules
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 
+// setup app and port
 const app = express()
 const port = process.env.PORT || 5000
 
+// express middleware
 app.use(cors())
 app.use(express.json())
 
+// connect to db
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true })
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('MongoDB database connection successful')
+})
+
+// start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
