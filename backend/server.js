3913927b1f1b70ee2af3fecf09bae074aa329b61
Biggs Thorarensen <biggs@devillion.com>
2020-01-24 11:12:06 +0000

    endpoints for exercises and users

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,13 @@ connection.once('open', () => {
     console.log('MongoDB database connection successful')
 })
 
+// routes
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
 // start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
